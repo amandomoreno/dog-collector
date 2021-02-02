453a6ab3a4463241661aeb05ef4082aae5c1d17b
amandomoreno <amandomoreno47@gmail.com>
2021-02-02 16:11:29 -0600

    class Toys

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Dog, VetVisit
+from .models import Dog, VetVisit, Toy
 
 # Register your models here
 admin.site.register(Dog)
 admin.site.register(VetVisit)
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 # Add the following import
 from django.shortcuts import render, redirect
-from .models import Dog
+from .models import Dog, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import VetVisitForm
 
 def home(request):
@@ -16,8 +17,9 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
   vetvisit_form = VetVisitForm()
-  return render(request, 'dogs/detail.html', { 'dog': dog, 'vetvisit_form' : vetvisit_form })\
+  return render(request, 'dogs/detail.html', { 'dog': dog, 'vetvisit_form' : vetvisit_form, 'toys': toys_dog_doesnt_have })
 
 def add_vetvisit(request, dog_id):
   form = VetVisitForm(request.POST)
@@ -38,3 +40,26 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
   model = Dog
   success_url = '/dogs/'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
